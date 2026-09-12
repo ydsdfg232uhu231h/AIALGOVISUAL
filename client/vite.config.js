483df0507117eae9ece27f5d34fe221fd1eeb306
ydsdfg232uhu231h <yuvrajgupta90334@gmail.com>
2026-09-12 13:15:12 +0530

    logo update

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    proxy: {
-      '/api': 'https://aialgovisual-1.onrender.com',
-    },
-  },
+  base: "/"
 })
