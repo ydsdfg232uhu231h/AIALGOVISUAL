d308424d01db8ff51827617086d8663b540bfb5c
ydsdfg232uhu231h <yuvrajgupta90334@gmail.com>
2026-09-12 12:45:34 +0530

    server update

--- backend/app.js
+++ backend/app.js
@@ -1,27 +1,41 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import mrouter from "../backend/routes/index.routes.js";
 import path from "path";
 import { fileURLToPath } from "url";
+
 dotenv.config();
+
 const app = express();
 
-const __firstname = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__firstname);
-const distPath = path.join(__dirname, "..", "client", "dist");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(cors({ origin : "https://aialgovisual-1.onrender.com",
-  credentials: true
-    }));
+app.use(express.urlencoded({ extended: false }));
+
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "https://aafpsw.netlify.app",
+    credentials: true,
+  })
+);
+
 app.use(cookieParser(process.env.COOKIES_SECRET));
-app.use("/api/v1/",mrouter);
-app.use(express.static(path.join(__dirname, "../client" ,"dist")));
+
+app.use("/api/v1/", mrouter);
+
+
+const distPath = path.join(__dirname, "..", "client", "dist");
+
+
+app.use(express.static(distPath));
 
 app.get(/^(?!\/api\/v1).*/, (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.js"));
+  res.sendFile(path.join(distPath, "index.html"));
 });
 
 export default app;
