2dd5f65bd063b1a475cf23c6b3b491a17b065e32
ydsdfg232uhu231h <yuvrajgupta90334@gmail.com>
2026-09-12 11:09:43 +0530

    Deployment update 6

--- backend/client/vite.config.js
+++ backend/client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server:{
     proxy: {
-      '/api': 'http://loclhost:5000',
+      '/api': 'https://aialgovisual-1.onrender.com',
     },
   },
 })
