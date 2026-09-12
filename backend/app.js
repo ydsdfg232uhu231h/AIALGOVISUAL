ea9da5b0dd5f10e7a6cea5b3f334a78e87a5bdcd
ydsdfg232uhu231h <yuvrajgupta90334@gmail.com>
2026-09-12 12:38:35 +0530

    server update

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ app.use("/api/v1/",mrouter);
 app.use(express.static(path.join(__dirname, "../client" ,"dist")));
 
 app.get(/^(?!\/api\/v1).*/, (req, res) => {
-  res.sendFile(distPath);
+  res.sendFile(path.join(distPath, "index.js"));
 });
 
 export default app;
