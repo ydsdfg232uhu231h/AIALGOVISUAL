792868c1d2aea5aea126271a89fc4deafe4618dc
ydsdfg232uhu231h <yuvrajgupta90334@gmail.com>
2026-09-12 11:34:45 +0530

    Deployment update 7

--- backend/app.js
+++ backend/app.js
@@ -32,10 +32,10 @@ app.use(cors({
   }));
 app.use(cookieParser(process.env.COOKIES_SECRET));
 app.use("/api/v1/",mrouter);
-app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static(path.join(__dirname, "dist")));
 
 app.get(/^(?!\/api\/v1).*/, (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 export default app;
