b07637a5a919d4bdbd63da59b2c12866be8a2112
ydsdfg232uhu231h <yuvrajgupta90334@gmail.com>
2026-09-21 20:28:56 +0530

    UI Big Update

--- backend/controller/notification.controller.js
+++ backend/controller/notification.controller.js
@@ -2,7 +2,7 @@ import nodemailer from "nodemailer";
 import fs from "fs";
 import path from "path";
 // 1. Initialize Nodemailer transporter with Gmail SMTP
-const templatePath = path.join(process.cwd() , "backend","view",  "notification.html");
+const templatePath = path.join(process.cwd() ,"view",  "notification.html");
 let htmlContent = fs.readFileSync(templatePath, "utf-8");
 const transporter = nodemailer.createTransport({
   service: "gmail",
